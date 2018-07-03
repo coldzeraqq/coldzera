8cf3c2c96e923970c7116d56f865cfda43466be4
fgy <fgy@dankal.cn>
2018-07-03 16:40:26 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Coldzeras
+                    Coldzera
                 </div>
 
                 <div class="links">
